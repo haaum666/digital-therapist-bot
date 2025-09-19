dd156cde6b6ce83364fa7bcee1a1d8d5e43c933d
haaum666 <moiraidrone@gmail.com>
2025-09-19 19:51:11 +0300

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,1 +1,13 @@
+import { InlineKeyboard } from "grammy";
 
+const startHandler = async (ctx) => {
+  const inlineKeyboard = new InlineKeyboard()
+    .text("Полная диагностика", "full_diagnosis")
+    .text("Проверка по блокам", "by_blocks");
+
+  await ctx.reply("Привет! Я Digital-терапевт для бизнеса, и я готов провести быструю диагностику вашего бизнеса. Мы можем пойти двумя путями: Полная диагностика или Проверка по блокам.", {
+    reply_markup: inlineKeyboard
+  });
+};
+
+export default startHandler;
