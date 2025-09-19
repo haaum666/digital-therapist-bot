62922800dd49c39360f8acbea7a7f716b23d149c
haaum666 <moiraidrone@gmail.com>
2025-09-19 19:51:50 +0300

    Update buttons.js

--- src/handlers/buttons.js
+++ src/handlers/buttons.js
@@ -1,1 +1,14 @@
+// src/handlers/buttons.js
+import { startDialog } from "./dialog.js";
 
+const buttonsHandler = async (ctx) => {
+  const callbackQuery = ctx.callbackQuery.data;
+
+  // Проверка на нажатие кнопки "Полная диагностика"
+  if (callbackQuery === "full_diagnosis") {
+    // Начало диалога полной диагностики
+    await startDialog(ctx, "full_diagnosis");
+  }
+};
+
+export default buttonsHandler;
