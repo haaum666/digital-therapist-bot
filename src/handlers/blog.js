443650d07689156129c6cf3e839ae645ddc54c49
haaum666 <moiraidrone@gmail.com>
2025-09-20 23:34:09 +0300

    Update blog.js

--- src/handlers/blog.js
+++ src/handlers/blog.js
@@ -1,8 +1,19 @@
 // src/handlers/blog.js
 const handleBlog = async (ctx) => {
     await ctx.reply(
-        `Здесь вы можете найти полезные статьи и кейсы по цифровизации бизнеса:
-https://quantumdev.ru/blog`
+        `Подписывайся на наши медиа, чтобы получать полезные статьи и кейсы по цифровизации бизнеса. Выбирай, где тебе удобнее читать:`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Блог на сайте', url: 'https://quantum-dev.ru/blog' },
+                    ],
+                    [
+                        { text: 'Канал в Telegram', url: 'https://t.me/quantumdev1' },
+                    ],
+                ],
+            },
+        }
     );
 };
 
