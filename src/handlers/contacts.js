54d67950f59e08b1735377b1d2c92738ca02fbdb
haaum666 <moiraidrone@gmail.com>
2025-09-21 15:54:30 +0300

    Update contacts.js

--- src/handlers/contacts.js
+++ src/handlers/contacts.js
@@ -1,16 +1,12 @@
-// src/handlers/contacts.js
 const handleContacts = async (ctx) => {
     await ctx.reply(
         `Если ты хочешь получить консультацию или у тебя есть вопросы, мы всегда на связи. Выбери, где тебе удобнее:`,
         {
             reply_markup: {
                 inline_keyboard: [
-                    [
-                        { text: 'Telegram', url: 'https://t.me/Quantumdevelop' },
-                    ],
-                    [
-                        { text: 'WhatsApp', url: 'https://wa.me/79994512356' },
-                    ],
+                    [{ text: 'Telegram', url: 'https://t.me/Quantumdevelop' }],
+                    [{ text: 'WhatsApp', url: 'https://wa.me/79994512356' }],
+                    [{ text: 'Вернуться в начало', callback_data: 'show_main_menu' }], // <-- Добавлено
                 ],
             },
         }
